88da4927a4b66b8ac9352c74a03915d9a6d8b00c
MaayooMoh <maya.hashem1505@gmail.com>
2025-04-26 00:53:34 +0300

    Create Customer.java

---
+++ Customer.java
@@ -0,0 +1,36 @@
+public class Customer {
+    private String name;
+    private String phone;
+    private String id;
+
+    public Customer(String name, String phone, String id) {
+        this.name = name;
+        this.phone = phone;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
